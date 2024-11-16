ea5e9d5f6032628e2147202e711f74e1ff6c3a8d
Johannes Rolfes <rolfes.games@web.de>
2024-11-16 21:21:00 +0100

    file structuring and inetraging file read into xml

--- genCircCorrds.py
+++ genCircCorrds.py
@@ -1,6 +1,21 @@
 import numpy as np
 
-def generate_circle_points(rotangle, radius, height, orOffset, num_points, duration, lockorientation=False):
+
+def generate_orientations(rotangle, orOffset, num_points, duration, lockorientation=False):
+    angle = rotangle / num_points
+    angles = np.linspace(0, rotangle + angle, num_points+1, endpoint=False)
+    # assuming equidistant sampling
+    time_interval = duration / num_points
+
+    orientation = []
+    if lockorientation:
+        for i in range(num_points+1):
+            t = i * time_interval
+            orientation.append((t, (angles[i]/np.pi)*180 + orOffset, 0, 0))
+    return orientation
+
+
+def generate_circle_points(rotangle, radius, height, num_points, duration):
     angle = rotangle / num_points
     angles = np.linspace(0, rotangle + angle, num_points+1, endpoint=False)
     x_coords = radius * np.cos(angles)
@@ -10,17 +25,11 @@ def generate_circle_points(rotangle, radius, height, orOffset, num_points, durat
     time_interval = duration / num_points
 
     points = []
-    orientation = []
     for i in range(num_points+1):
         t = i * time_interval
         x, y = x_coords[i], y_coords[i]
         z = z_coords  # Since we are in 2D space
         points.append((t, x, y, z))
-    if lockorientation:
-        for i in range(num_points+1):
-            t = i * time_interval
-            orientation.append((t, (angles[i]/np.pi)*180 + orOffset, 0, 0))
-        points = np.vstack((points, orientation))
     return points
 
 def save_to_file(filename, points):
@@ -34,14 +43,18 @@ def save_to_file(filename, points):
 # ----------------------------------------
 
 radius = 3                # Radius of the circle
-rotangle = 3 * (2 * np.pi)
+num_rotations = 1
+rotangle = num_rotations * (2 * np.pi)
 points_per_rot = 200
-num_points = points_per_rot*(rotangle/2*np.pi)          # Number of points along the circle
-speed = 20                # duration of one rotation
-height = 1.5
+num_points = int(round(points_per_rot*(rotangle/2*np.pi), 0))          # Number of points along the circle
+duration = 20                # duration of one rotation
+height = 1.6
 
 lock = True 
 orOffset = -90
 
-data = generate_circle_points(rotangle, radius, height, orOffset, num_points, speed, lock)
-save_to_file("circle_path.txt", data)
+points = generate_circle_points(rotangle, radius, height, num_points, duration)
+orientations = generate_orientations(rotangle, orOffset, num_points, duration, lock)
+
+save_to_file(f"./scenes/paths/circle_test.txt", points)
+save_to_file(f"./scenes/paths/orientations_test.txt", orientations)

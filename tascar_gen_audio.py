a5f76b56bfe84e58c0aa7797b15b09b4244ce9f6
Johannes Rolfes <rolfes.games@web.de>
2024-12-06 10:47:07 +0100

    render tascar outputs

--- tascar_gen_audio.py
+++ tascar_gen_audio.py
@@ -7,15 +7,15 @@ def run_command(command):
     except subprocess.CalledProcessError as e:
         print(e)
 
-tascar_dir = "/Users/johannesrolfes/Desktop/StudiumHA/Uni/Sem2/Projekt"
 #tascar_dir = "path"
+tascar_dir = "/Users/johannesrolfes/Desktop/StudiumHA/Uni/Sem2/Projekt/tascar"
 cd = f"cd {tascar_dir}"
+print(f"Running: {cd}")
+run_command(cd)
 
 # render tascar files
 out_dir = "/Users/johannesrolfes/Desktop/StudiumHA/Uni/Sem2/Projekt/repo/MovingSources/rendered_audio_files"
-commands = ["tascar_renderfile -o S0N0.wav -s tascar_scene.tsc "]
-
+commands = [f"tascar_renderfile -o {out_dir}/S0N0.wav -s tascar_scene.tsc "]
 for command in commands:
-    stdout, stderr = run_command(command)
-    print("Output:", stdout)
-    print("Errors:", stderr)
+    print(f"Running: {command}")
+    run_command(command)

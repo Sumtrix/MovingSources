6f412a1f25e05788c8b939fde06601b663026ad8
Johannes Rolfes <rolfes.games@web.de>
2025-01-30 12:12:12 +0100

    anlge plot

--- plot_LE_results_all.py
+++ plot_LE_results_all.py
@@ -197,6 +197,14 @@ for subj_name in subj_names:
 
     #------------------------------------------------
     # # S0N270N90 headrot
+    select_indices = [4]
+    scene_conditions = [scenes_med_7[select_indices], 
+                        scenes_med_10[select_indices], 
+                        scenes_slow_7[select_indices], 
+                        scenes_slow_10[select_indices]]
+    plotConditions(scene_conditions, numLabels=24)
+
+
     # select_indices = [4]
     # scene_conditions = [scenes_med_7[select_indices], 
     #                     scenes_med_10[select_indices]]
@@ -206,40 +214,3 @@ for subj_name in subj_names:
     #                     scenes_slow_10[select_indices]]
     # plotConditions(scene_conditions, allInOne=True)
     
-    # plot comparison S0N0 S0N90 S0Nrot
-    select_indices = [4]
-    scene_conditions = [scenes_med_7[select_indices], 
-                        scenes_med_10[select_indices], 
-                        scenes_slow_7[select_indices], 
-                        scenes_slow_10[select_indices]]
-    plotConditions(scene_conditions, numLabels=24)
-
-
-
-
-
-
-# #results_path = "./le_modell_results/-3 dB SNR"
-# results_path = "C:/Users/annik/OneDrive/Desktop/Projekt/real-time-assessment-listening-effort/measurement/Results/JR Test"
-# #files = sorted(glob.glob(os.path.join(results_path, "*medium*.npy")))
-# files = os.listdir(results_path)
-
-
-# fig, ax = plt.subplots(2, 1)
-# legend = []
-# print(files)
-# for file in files:
-#     print(file)
-#     data_list = np.load(file)  # Load data from .npy file
-#     scene = os.path.basename(file)
-#     num_values = len(data_list)
-#     ax[0].plot(data_list)
-#     legend.append(scene)
-# ax[0].legend(legend)
-
-        
-# #ax_pol.legend(legend_vec)
-# ax[0].set_ylim([0, 14])
-# ax[0].grid(linestyle=':', alpha=0.5)
-# plt.show()
-
